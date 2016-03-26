f3ff96cf9a790a86fcd488187bc55d341e79e1fc
shaunakv1 <shaunakv1@gmail.com>
2016-03-26 10:29:14 -0400

    renamed the bot

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Telegram Bot' });
+  res.render('index', { title: 'TeeBot' });
 });
 
 module.exports = router;
