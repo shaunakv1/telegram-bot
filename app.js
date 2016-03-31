bbbaac6f2408f191f7b80954a760611c1aea2cbc
shaunakv1 <shaunakv1@gmail.com>
2016-03-30 23:10:48 -0400

    mounted mongoose, passport, and session-store and created a model for accounts

--- app.js
+++ app.js
@@ -4,11 +4,22 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+//setup session store
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+//connect mongoose
+mongoose.connect('mongodb://localhost/passport_local_mongoose_express4');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,11 +31,27 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+    secret: 'foo',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({
+        mongooseConnection: mongoose.connection
+    })
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+// passport config
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
